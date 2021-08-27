02ee1238d6d9472140bda8cb4ff8a971a2abe0d6
MansourRumman <88175201+MansourRumman@users.noreply.github.com>
2021-08-27 11:31:51 -0600

    se creo el projecto

---
+++ lab5P2_MansourRumman/src/lab5p2_mansourrumman/Lab5P2_MansourRumman.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab5p2_mansourrumman;
+
+/**
+ *
+ * @author manso
+ */
+public class Lab5P2_MansourRumman {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
